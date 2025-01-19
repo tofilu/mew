23b7c7af1dd47c69cd653e933e42ce4367780d8d
tofilu <finnlunderup@gmail.com>
2025-01-19 18:10:48 +0100

    Integrated Scheduled Notification and deleted obsolete code

--- lib/Home.dart
+++ lib/Home.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'AddDrugScreen.dart';
 import 'DailyScreen.dart';
 import 'DrugPlanScreen.dart';
-import 'Helper/NotificationService.dart';
 
 class Home extends StatefulWidget {
   final int index;
@@ -36,38 +35,10 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    final notificationService =
-        NotificationService(); // Create a single instance
-
-    var timeNow = DateTime.now();
-    var time = DateTime.now().add(Duration(seconds: 5));
 
     return Scaffold(
       appBar: AppBar(
         title: Text('MEW'),
-        actions: [
-          ElevatedButton(
-            onPressed: () async {
-              // Test notification
-              try {
-                await notificationService.showNotification(
-                  title: 'Instant Notification',
-                  body: 'Instant Notification at: $timeNow',
-                );
-                print('pressed button');
-                await notificationService.scheduleNotification(
-                  id: 1,
-                  title: 'Scheduled Notification',
-                  body: 'Scheduled Notification for: $time',
-                  scheduleTime: DateTime.now().add(Duration(seconds: 5)),
-                );
-              } catch (e) {
-                print('ERROR NOTI: $e');
-              }
-            },
-            child: Text('Noti Test'),
-          ),
-        ],
       ),
       bottomNavigationBar: NavigationBar(
         onDestinationSelected: (int index) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,14 @@
-import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:flutter/material.dart';
-import 'package:mew/Helper/AlarmSetUp.dart';
 import 'package:mew/theme/mewTheme.dart';
+import 'Helper/NotificationService.dart';
 import 'Home.dart';
 
 
 void main() async {
   // Ensure that plugin services are initialized so that `AndroidAlarmManager` is registered
   WidgetsFlutterBinding.ensureInitialized();
-  // Initialize AndroidAlarmManager
-  await AndroidAlarmManager.initialize();
-  AlarmSetUp().setUpAlarmMidnight();
 
-  await notificationService.initNotification();
+  await NotificationService.instance.initNotification();
 
   runApp(const MewApp());
 }
