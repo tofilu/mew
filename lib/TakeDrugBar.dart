e5e9ba964a6d2b50216e3a68c7bf4b9d7eeff7e5
ginarenoldi <132135090+ginarenoldi@users.noreply.github.com>
2025-01-20 11:48:33 +0100

    implemented pop up for taking medications

--- lib/TakeDrugBar.dart
+++ lib/TakeDrugBar.dart
@@ -11,12 +11,56 @@ class TakeDrugBar extends DrugBar {
 
   @override
   buildDrugAlarm(BuildContext context) {
-    return Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-      Column(children: [Text(drug.name)]),
-      Column(children: [Text(drug.time)]),
-      Column(children: [
-        ElevatedButton(onPressed: () {}, child: Icon(Icons.medication_liquid))
-      ])
-    ]);
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: [
+        Column(children: [Text(drug.name)]),
+        Column(children: [Text(drug.time)]),
+        Column(children: [
+          ElevatedButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (BuildContext context) {
+                  return _buildPopUp(context);
+                },
+              );
+            },
+            child: Icon(Icons.medication_liquid),
+          ),
+        ]),
+      ],
+    );
+  }
+
+  _buildPopUp(BuildContext context) {
+    return AlertDialog(
+      title: Text("Medication Reminder"),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        // Verhindert übergroßen Dialog
+        children: [
+          Text("Take medication"),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop(); // Schließt den Dialog
+                },
+                child: Text("Skip"),
+              ),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.of(context).pop(); // Schließt den Dialog
+                },
+                child: Text("Done"),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
