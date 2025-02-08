def8c1014452c458675022eb637b7478d9ecae7e
tofilu <finnlunderup@gmail.com>
2025-02-08 11:34:04 +0100

    Added swip animation

--- lib/DrugBar.dart
+++ lib/DrugBar.dart
@@ -8,15 +8,43 @@ abstract class DrugBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
+    return Dismissible(
+      key: Key(drug.hashCode.toString()), // Eindeutiger Schlüssel für Dismissible
+      direction: DismissDirection.horizontal, // Links oder rechts wischen
+      background: Container(
+        color: Colors.red, // Hintergrundfarbe beim Wischen
+        alignment: Alignment.centerLeft,
+        padding: EdgeInsets.only(left: 20),
+        child: Icon(Icons.delete, color: Colors.white, size: 30), // Icon links
+      ),
+      secondaryBackground: Container(
+        color: Colors.red,
+        alignment: Alignment.centerRight,
+        padding: EdgeInsets.only(right: 20),
+        child: Icon(Icons.delete, color: Colors.white, size: 30), // Icon rechts
+      ),
+      onDismissed: (direction) {
+        // Hier kannst du die Löschlogik implementieren
+        print("${drug} wurde gelöscht");
+      },
+      child: Card(
         elevation: 5.0,
         color: Colors.grey,
-        shape: BeveledRectangleBorder(),
-        child: ListBody(children: [
-          Padding(
+        shape: RoundedRectangleBorder(
+            side: BorderSide.none,
+            borderRadius: BorderRadius.circular(15.0)
+        ),
+        clipBehavior: Clip.antiAlias,
+        child: ListBody(
+          children: [
+            Padding(
               padding: const EdgeInsets.only(bottom: 15.0, top: 15.0),
-              child: buildBar(context))
-        ]));
+              child: buildBar(context),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 
   Widget buildBar(BuildContext context);
